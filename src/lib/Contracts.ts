b31062abfe226c571a0a1a4d06428ecc32bd3e93
Phillip Goldberg <phillip.goldberg.7@gmail.com>
2018-07-19 14:40:46 -0700

    none of this shit works, but heres history of it happening

--- src/lib/Contracts.ts
+++ src/lib/Contracts.ts
@@ -3,7 +3,6 @@ import { Proxy as ProxyContract } from '@dydxprotocol/protocol';
 import { ERC20ShortCreator as ERC20ShortCreatorContract } from '@dydxprotocol/protocol';
 import { ERC20LongCreator as ERC20LongCreatorContract } from '@dydxprotocol/protocol';
 import { SharedLoanCreator as SharedLoanCreatorContract } from '@dydxprotocol/protocol';
-import { TestToken as TestTokenContract } from '@dydxprotocol/protocol';
 import contract from 'truffle-contract';
 import { setupContract } from './Helpers';
 
@@ -13,20 +12,12 @@ const ERC20ShortCreator = contract(ERC20ShortCreatorContract);
 const ERC20LongCreator = contract(ERC20LongCreatorContract);
 const SharedLoanCreator = contract(SharedLoanCreatorContract);
 
-const TokenA = contract(TestTokenContract);
-const TokenB = contract(TestTokenContract);
-const Vault = contract(TestTokenContract);
-
 export class Contracts {
     public margin;
     public proxy;
     public erc20ShortCreator;
     public erc20LongCreator;
     public sharedLoanCreator;
-
-    public tokenA;
-    public tokenB;
-    public vault;
 
     constructor(
         provider: any,
@@ -52,30 +43,18 @@ export class Contracts {
         setupContract(ERC20LongCreator, provider, networkId);
         setupContract(SharedLoanCreator, provider, networkId);
 
-        setupContract(TokenA, provider, networkId);
-        setupContract(TokenB, provider, networkId);
-        setupContract(Vault, provider, networkId);
-
-
         const [
             margin,
             proxy,
             erc20ShortCreator,
             erc20LongCreator,
-            sharedLoanCreator,
-            tokenA,
-            tokenB,
-            vault
+            sharedLoanCreator
         ] = await Promise.all([
             Margin.deployed(),
             Proxy.deployed(),
             ERC20ShortCreator.deployed(),
             ERC20LongCreator.deployed(),
-            SharedLoanCreator.deployed(),
-
-            TokenA.deployed(),
-            TokenB.deployed(),
-            Vault.deployed(),
+            SharedLoanCreator.deployed()
         ]);
 
         this.margin = margin;
@@ -83,9 +62,5 @@ export class Contracts {
         this.erc20ShortCreator = erc20ShortCreator;
         this.erc20LongCreator = erc20LongCreator;
         this.sharedLoanCreator = sharedLoanCreator;
-
-        this.tokenA = tokenA;
-        this.tokenB = tokenB;
-        this.vault = vault;
     }
 }
