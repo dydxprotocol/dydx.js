869776b68d14dc4962234976a4ca0d20aa786e08
Phillip Goldberg <phillip.goldberg.7@gmail.com>
2018-07-26 19:24:35 -0700

    Revert "remove expecthelper"
    This reverts commit 270164d0fcc74d063e2699b22fe419626d3539d5.

---
+++ __testing__/helpers/ExpectHelper.ts
@@ -0,0 +1,11 @@
+import chai from 'chai';
+const expect = chai.expect;
+// For solidity function calls that violate require()
+export async function expectThrow(promise) {
+  try {
+    await promise;
+    throw new Error('Did not throw');
+  } catch (e) {
+    expect(e).to.match(/Exception while processing transaction: revert/);
+  }
+}
